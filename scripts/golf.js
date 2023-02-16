ed9693f6942a4a32d83f5c65ffd0b2badc49cfcf
Jacob Campbell <103970928+nimasii@users.noreply.github.com>
2023-02-16 19:11:31 +0000

    Added collisions to the golf game

--- scripts/golf.js
+++ scripts/golf.js
@@ -9,6 +9,7 @@ function startGame() {
     wall = new component(100, 300, 'black', 500, 0)
     power = new component(200, 50, 'black', 800, 450)
     slide = new component(2, 49, 'blue', 801, 451)
+    aim = new component (5, 100, 'white', (ball.x + 7.5), (ball.y + 10))
 }
 
 var myGameArea = {
@@ -33,6 +34,7 @@ function component(width, height, color, x, y) {
     this.speedX = 0;
     this.speedY = 0;
     this.size = 0;
+    this.bounce = 0;
     this.increasing = true;
     this.shoot = false;
     this.update = function() {
@@ -62,29 +64,70 @@ function component(width, height, color, x, y) {
         }
     }
     this.newPos = function() {
-        if (shoot == true && this.speedX > 0) {
+        if (shoot == true && (this.speedX > 0.3 || this.speedX < 0.2)) {
             this.x += this.speedX;
-            this.speedX -= .2;
-            if (this.speedX < 0) {
+            if (this.speedX > 0) {
+                this.speedX -= .2;
+            } else if (this.speedX < 0) {
+                this.speedX += .3;
+            } else {
                 this.speedX = 0;
             }
         } else {
             shoot = false
+            this.speedX =0;
         }
     }
-    this.collide = function() {
+    this.collide = function(wall) {
+        var ballLeft = this.x;
+        var ballRight = this. x + this.width;
+        var ballTop = this.y + this.height;
+        var ballBottom = this.y;
+        var wallLeft = wall.x;
+        var wallRight = wall.x + wall.width;
+        var wallTop = wall.y;
+        var wallBottom = wall.y + wall.height;
+        var crash = true;
 
+        if ((ballBottom < wallTop) || (ballTop > wallBottom) || (ballRight < wallLeft) || (ballLeft > wallRight)) {
+            crash = false;
+        } 
+        return crash;
+    }
+    this.boundary = function(wall) {
+        var ballLeft = this.x;
+        var ballRight = this. x + this.width;
+        var ballTop = this.y + this.height;
+        var ballBottom = this.y;
+        var wallLeft = 0 + this.width;
+        var wallRight = wall.width;
+        var wallTop = wall.height;
+        var wallBottom = 0 + this.width;
+        var crash = true;
+
+        if ((ballBottom > wallTop) || (ballTop < wallBottom) || (ballRight > wallLeft) || (ballLeft < wallRight)) {
+            crash = false;
+        } 
+        return crash;
     }
   }
 
 function updateGameArea() {
     myGameArea.clear();
+    if (ball.collide(wall) || ball.boundary(myGameArea)) {
+        ball.speedX = ball.speedX * -1;
+        ball.speedY = ball.speedY * -1;
+    }
+    if (ball.collide(hole)) {
+        console.log('You Win!')
+    }
     ball.newPos();
     ball.update();
     hole.update();
     wall.update();
     power.update();
     slide.slideUpdate();
+    aim.update();
 }
 
 function takeShot() {
